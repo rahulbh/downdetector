ec1d05e8aa68e0f496e498460646965458c43d08
Rahul Bharti <rahulbh95@gmail.com>
2024-12-09 23:49:14 +0800

    All Files

---
+++ src/main/kotlin/com/down/downdetector/DowndetectorApplication.kt
@@ -0,0 +1,11 @@
+package com.down.downdetector
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DowndetectorApplication
+
+fun main(args: Array<String>) {
+	runApplication<DowndetectorApplication>(*args)
+}
---
+++ src/test/kotlin/com/down/downdetector/DowndetectorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.down.downdetector
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DowndetectorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
